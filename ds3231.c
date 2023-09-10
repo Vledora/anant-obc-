2b15e12a93be6f61f8cbaead7d7b593aa253d9d7
Kartik Yadav <137162791+Vledora@users.noreply.github.com>
2023-09-10 17:10:59 +0530

    ds3231.c

--- ds3231.c
+++ ds3231.c
@@ -5,17 +5,29 @@
 #include<sys/ioctl.h>
 #include<linux/i2c-dev.h>
 #include<sys/stat.h>
+#include<math.h>
 
 #define i2c "/dev/i2c-2"
 
 uint8_t slave_addr = 0b1101000;
 uint8_t data = 0x00;
-uint8_t reply;
-uint8_t *time = data;
+uint8_t reply[3];
 
+int BcdToDecimal(uint8_t arg){
+	int ans = 0;
+    for(int i=0;i<8;++i){
+	if(i<=3){
+	    ans += pow(i, 2) * ((arg >> i) % 2);
+	}
+	else{
+	    ans += 10 * (pow(i-4, 2) * ((arg >> i) % 2));
+	}
+    }
+    return ans;
+}
 static int write(int fd, const void *buf, size_t count){
 	if((cb = write(fd,buf,count))>0){
-		print("%s file opened to write",__FILENAME__);
+		print("file opened to write");
 		return 0;
 	}
 	else {perror("error");
@@ -24,7 +36,7 @@ static int write(int fd, const void *buf, size_t count){
 
 static int read(int fd, const void *buf, size_t count){
 	if((cb = write(fd,buf,count))>0){
-		print("%s file opened to read",__FILENAME__);
+		print("file opened to read");
 		return 0;
 	}
 	else {perror("error");
@@ -39,14 +51,17 @@ static int time(){
 	if ((ioctl(fd,I2C_SLAVE,slave_addr))<0)
 	{
 		perror("error");
-		close(fd);
 		return(-1);
 	}
 	uint8_t *reply;
+	write(fd,data,1);
 	for(int i;i<3,i++){
-	write(fd,time+i,1);
 	read(fd,reply+i,1);}
-	
+	int s,m,h;
+	s = bcd_to_decimal(reply[1]);
+	m = bcd_to_decimal(reply[2]);
+	h = bcd_to_decimal(reply[3]);
+	snprintf(time, 10,"%dh:%dm:%ds",h,m,s);
 	close(fd);
 	return 0;
 }
